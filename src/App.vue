269d1b00e2fb328c1868d2deb51ae6c18e087a3d
Tim Jarratt <tjarratt@gmail.com>
2022-07-10 18:16:00 +0200

    Build a basic grid vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Birdle</h1>
+  <WordGrid />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WordGrid from './components/WordGrid.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WordGrid,
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  background-color: #121213;
+}
 #app {
+  margin: 0;
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/WordGrid.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id=wrapper>
+    <div class=grid>
+      <div class=row>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+      </div>
+      <div class=row>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+      </div>
+      <div class=row>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+        <div class=row-item>A</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'WordGrid',
+  props: { },
+}
+
+</script> 
+
+<style scoped>
+
+#wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.grid {
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  grid-gap: 5px;
+  padding: 10px;
+}
+
+.row {
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: repeat(5, 1fr);
+  
+}
+
+.row-item {
+  display: grid;
+  grid-gap: 5px;
+
+  width: 7em;
+  height: 7em;
+  border: 2px solid #3A3A3C;
+}
+
+</style>
